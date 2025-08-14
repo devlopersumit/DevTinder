e3b6bb9fe80f28b29e2b0f14d39a78420b5da0a2
devlopersumit <sumitsj2002@gmail.com>
2025-08-14 16:28:29 +0530

    exploring GET, POST, PATCH, DELETE http request

--- src/app.js
+++ src/app.js
@@ -2,16 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.use("/about",(req, res) => {
-    res.send("Kya bolti public")
+// app.use("/about",(req, res) => {
+//     res.send("Kya bolti public")
+// });
+
+// app.use("/contact",(req, res) => {
+//     res.send("My Contact number is 9876543210")
+// });
+
+// app.use("/",(req, res) => {
+//     res.send("Amma behen pe aa jaunga mai")
+// });
+
+app.get("/user", (req, res) => {
+    res.send({fisrtName: "Sumit",
+        lastName: "Jha"
+    })
 });
 
-app.use("/contact",(req, res) => {
-    res.send("My Contact number is 9876543210")
+app.post("/user", (req, res) => {
+    res.send("Data stored in DB successfully")
 });
 
-app.use("/",(req, res) => {
-    res.send("Amma behen pe aa jaunga mai")
+app.delete("/user", (req, res) => {
+    res.send("Data deleted successfully")
+});
+
+app.patch("/user", (req, res) => {
+    res.send("Updated the database successfully")
 });
 
 app.listen(4000, () => {

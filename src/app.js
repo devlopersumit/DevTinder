1d2082ca1247e9a81eef8d6030a0bb66d43a8ba4
devlopersumit <sumitsj2002@gmail.com>
2025-08-18 16:30:46 +0530

    exploring

--- src/app.js
+++ src/app.js
@@ -1,41 +1,15 @@
 const express = require("express");
 const app = express();
 
-// app.use("/about",(req, res) => {
-//     res.send("Kya bolti public")
-// });
-// app.use("/contact",(req, res) => {
-//     res.send("My Contact number is 9876543210")
-// });
-// app.use("/",(req, res) => {
-//     res.send("Amma behen pe aa jaunga mai")
-// });
 
-// app.get("/user/:userId/:name", (req, res) => {
-//   console.log(req.params);
-
-//   res.send({ fisrtName: "Sumit", lastName: "Jha" });
-// });
-
-// app.post("/user", (req, res) => {
-//   res.send("Data stored in DB successfully");
-// });
-
-// app.delete("/user", (req, res) => {
-//   res.send("Data deleted successfully");
-// });
-
-// app.patch("/user", (req, res) => {
-//   res.send("Updated the database successfully");
-// });
-
-app.use("/", (req, res, next) => {
+app.get("/", (req, res, next) => {
   // res.send("Welcome Back!")
   next();
-},
-(req, res) => {
-  res.send("I am the best!!!")
 });
+
+app.get("/", (req, res) => {
+  res.send("Sumit is best!!!")
+})
 
 app.listen(4000, () => {
   console.log("Server is running perfectly on port 4000");

771ae159f6ceff2997f2fcdae3c33cbebd2e9dbf
devlopersumit <sumitsj2002@gmail.com>
2025-08-18 17:07:40 +0530

    understood middlewares

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const {adminAuth} = require("./middlewares/adminAuth");
+const {userAuth} = require("./middlewares/userAuth");
+
 const app = express();
+ app.use(express.json());
 
+app.use("/admin", adminAuth)
 
-app.get("/", (req, res, next) => {
-  // res.send("Welcome Back!")
-  next();
+app.get("/admin/getAllData", (req, res) => {
+  console.log("Sending your all data");
+  res.send("Data has sent")
 });
 
-app.get("/", (req, res) => {
-  res.send("Sumit is best!!!")
+app.post("/admin/updateData", (req, res) => {
+  console.log("Updata the data...");
+  res.send("Data has updated")
+});
+
+app.get("/user/getUser", userAuth, (req, res) => {
+  res.send("Sending data...")
+})
+
+app.get("/user/login",(req, res) => {
+  res.send("Logging in...")
 })
 
 app.listen(4000, () => {
---
+++ src/middlewares/adminAuth.js
@@ -0,0 +1,14 @@
+const adminAuth = (req, res, next) => {
+  const token = "xyz";
+  const isAdminAuth = token === "xyz";
+  if(!isAdminAuth) {
+    res.status(401).send("Unauthorized Person")
+  }
+  else {
+    next()
+  }
+}
+
+module.exports = {
+    adminAuth,
+}
